4c5b4740fc6940bb86ae05cfce3ab28676624819
SamTombs <samuel.tombs91@outlook.com>
2025-08-04 16:07:03 +0200

    lab complete

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,21 @@
+const BurgerStack = ({ stack, removeFromBurger }) => {
+  return (
+    <div>
+      <h2>Your Burger</h2>
+      <ul>
+        {stack.map((ingredient, index) => (
+          <li key={index} style={{ backgroundColor: ingredient.color }}>
+            {ingredient.name}
+            <button
+              onClick={() => removeFromBurger(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BurgerStack;
